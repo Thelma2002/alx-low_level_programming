2a8c5d6721c3a6eb83f751c4d8eb1f20e1fef330
Thelma2002 <lungile13zulu@gmail.com>
2023-02-23 12:35:48 -0800

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,8 @@
-#ifendef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_reaining_days(int onth, int day, int year);
-int convert_day(int month, int day);
 
-#endif
+#endif /* MAIN_H */
